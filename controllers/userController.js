9a5eaa7178975d796f8e40857260f10a9b5e73ca
jacobuhlig <jacobuhlig@gmail.com>
2023-08-08 16:30:08 +0200

    Added verifyPassword function in the process of implementing the delete account functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,43 +1,49 @@
 import { User, Favorite } from '../database/models/index.js';
-
-const toBeReturned = ['id', 'username', 'email', 'role', 'created_at', 'updated_at']
-
-
+import { comparePassword } from '../services/passwordService.js';
+import { destroySession } from '../services/sessionService.js';
+const toBeReturned = [
+  'id',
+  'username',
+  'email',
+  'role',
+  'created_at',
+  'updated_at',
+];
 
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await User.findAll({attributes: toBeReturned});
+    const users = await User.findAll({ attributes: toBeReturned });
     return res.json(users);
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.toString() });
+    return res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.toString() });
   }
 };
-
-
 
 export const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const user = await User.findByPk(id, {attributes: toBeReturned});
+    const user = await User.findByPk(id, { attributes: toBeReturned });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
     return res.json(user);
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.toString() });
+    return res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.toString() });
   }
 };
-
-
 
 export const updateUserById = async (req, res) => {
   try {
     const { id } = req.params;
     const { username, email } = req.body;
 
-    const user = await User.findByPk(id, {attributes: toBeReturned});
+    const user = await User.findByPk(id, { attributes: toBeReturned });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -49,11 +55,11 @@ export const updateUserById = async (req, res) => {
 
     return res.json({ message: 'User updated successfully' });
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.toString() });
+    return res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.toString() });
   }
 };
-
-
 
 export const deleteUserById = async (req, res) => {
   try {
@@ -67,9 +73,27 @@ export const deleteUserById = async (req, res) => {
     // Delete user and associated favorites
     await Favorite.destroy({ where: { user_id: id } });
     await user.destroy();
+    destroySession(req, res);
 
     return res.json({ message: 'User deleted successfully' });
   } catch (error) {
-    return res.status(500).json({ message: 'Internal server error', error: error.toString() });
+    return res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.toString() });
+  }
+};
+
+export const verifyPassword = async (req, res) => {
+  const userId = req.params.id;
+  const inputPassword = req.body.password;
+
+  const user = await User.findByPk(userId);
+
+  const passwordMatch = await comparePassword(inputPassword, user.password);
+
+  if (passwordMatch) {
+    res.status(200).json({ success: true, message: 'Password verified.' });
+  } else {
+    res.status(401).json({ success: false, message: 'Invalid password.' });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import favoritesRoutes from './favorites.js';
 import userReviewRoutes from './userReviews.js';
-import { adminGuard, adminAndCurrentUserGuard } from '../middleware/auth-middleware.js';
-import { getAllUsers, getUserById, updateUserById, deleteUserById } from '../controllers/userController.js';
+import {
+  adminGuard,
+  adminAndCurrentUserGuard,
+} from '../middleware/auth-middleware.js';
+import {
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  verifyPassword,
+} from '../controllers/userController.js';
 
 const router = Router();
-
-
 
 // GET /users
 router.get('/', adminGuard, getAllUsers);
@@ -20,6 +27,8 @@ router.put('/:id', adminAndCurrentUserGuard, updateUserById);
 // DELETE /users/:id
 router.delete('/:id', adminAndCurrentUserGuard, deleteUserById);
 
+// POST /users/:id/verify-password
+router.post('/:id/verify-password', adminAndCurrentUserGuard, verifyPassword);
 
 // Sub-routes
 // Favorites
@@ -27,7 +36,5 @@ router.use('/:id/favorites', favoritesRoutes);
 
 // Reviews
 router.use('/:id/reviews', userReviewRoutes);
-
-
 
 export default router;
